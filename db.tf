462419eeb5a364d602fdf0b4e6598f02bbeecb3c
jiolloker <jiolloker@hotmail.com>
2023-05-15 13:10:14 -0300

    6try

--- db.tf
+++ db.tf
@@ -11,31 +11,24 @@ resource "azurerm_mysql_server" "db-server" {
   location            = azurerm_resource_group.db-rg.location
   version             = "5.7" # Choose the desired MySQL version
 
-  sku {
-    name     = "B_Gen5_1"
+
+    sku_name     = "B_Gen5_1"
     tier     = "basic"
     capacity = 1
     family   = "Gen5"
-  }
 
-  storage_profile {
-    storage_mb            = 32768 # Specify the desired storage size
-    backup_retention_days = 0     # Specify the desired backup retention period
-    geo_redundant_backup  = "Disabled"
-  }
 
-  infrastructure_encryption {
-    enabled = false # Disable infrastructure encryption
-  }
-
+  storage_mb            = 32768 # Specify the desired storage size
+  backup_retention_days = 0     # Specify the desired backup retention period
+  auto_grow_enabled                 = false
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
   delete_data_disks_on_deletion = true       # Delete data disks when the server is deleted
-  storage_auto_grow             = "Disabled" # Disable automatic storage growth
   backup_enforcement            = "Disabled" # Disable backup enforcement
-  diagnostics_settings {
-    log_analytics_destination {
-      enabled = false # Disable boot diagnostics
-    }
-  }
+
 
   administrator_login          = "adminuser" # Replace with your desired admin username
   administrator_login_password = var.db-pwd  # Replace with your desired admin password
