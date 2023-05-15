a8b9ce0f725d3de63d725b0d963c347352b70016
jiolloker <jiolloker@hotmail.com>
2023-05-15 13:11:31 -0300

    7try

--- db.tf
+++ db.tf
@@ -12,22 +12,19 @@ resource "azurerm_mysql_server" "db-server" {
   version             = "5.7" # Choose the desired MySQL version
 
 
-    sku_name     = "B_Gen5_1"
-    tier     = "basic"
-    capacity = 1
-    family   = "Gen5"
+  sku_name = "B_Gen5_1"
 
 
-  storage_mb            = 32768 # Specify the desired storage size
-  backup_retention_days = 0     # Specify the desired backup retention period
+
+  storage_mb                        = 32768 # Specify the desired storage size
+  backup_retention_days             = 0     # Specify the desired backup retention period
   auto_grow_enabled                 = false
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
   ssl_enforcement_enabled           = true
   ssl_minimal_tls_version_enforced  = "TLS1_2"
-  delete_data_disks_on_deletion = true       # Delete data disks when the server is deleted
-  backup_enforcement            = "Disabled" # Disable backup enforcement
+
 
 
   administrator_login          = "adminuser" # Replace with your desired admin username
